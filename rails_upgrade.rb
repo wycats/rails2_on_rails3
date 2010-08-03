652c28a3c0e1f9a987c5e0a056b43a4867564262
wycats <wycats@gmail.com>
2010-08-03 16:03:54 -0700

    Remove things moved to 3-0-stable

--- rails_upgrade.rb
+++ rails_upgrade.rb
@@ -55,20 +55,12 @@ require_bundler
 
 require "rails/all"
 require "active_support/all"
-
 require "rails_upgrade/action_view"
 
 # This is needed because Rails 3 requires development.rb, while Rails 2.3
 # invoked it in a special scope
 def self.config
   Rails.application.config
-end
-
-module Kernel
-  def returning(value)
-    yield value
-    value
-  end
 end
 
 module Rails
@@ -99,27 +91,21 @@ module Rails
       end
     end
 
-    MAJOR = VersionFaker.new(MAJOR, 2)
-    MINOR = VersionFaker.new(MINOR, 3)
-    TINY  = VersionFaker.new(TINY,  5)
+    def self.with_version(full_version)
+      new = full_version.split(".")
+      old = [MAJOR, MINOR, TINY]
+      class_eval "MAJOR, MINOR, TINY = #{new.inspect}"
+      yield
+    ensure
+      class_eval "MAJOR, MINOR, TINY = #{old.inspect}"
+    end
   end
 
   module Initializer
     def self.run(&block)
       klass = Class.new(Rails::Application)
       klass.instance_exec(klass.config, &block)
-      klass.config.autoload_paths << klass.root.join("lib")
-      Object.const_set(:RAILS_ROOT, klass.root.to_s)
       klass.initialize!
-    end
-  end
-
-  class Boot
-    def self.inherited(klass)
-      Rails.module_eval do
-        def self.boot!
-        end
-      end
     end
   end
 end
