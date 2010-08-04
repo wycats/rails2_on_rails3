b3ef03db39531e95091a03025b659aa434f675aa
wycats <wycats@gmail.com>
2010-08-04 01:06:45 -0700

    Use the correct APP_PATH based on whether application.rb exists

--- commands/server.rb
+++ commands/server.rb
@@ -1,6 +1,6 @@
 ARGV.unshift "server"
 
-APP_PATH = File.expand_path("../../../../config/environment.rb", __FILE__)
-$".push File.expand_path("../boot.rb", APP_PATH)
+root = File.expand_path("../../../../config", __FILE__)
+APP_PATH = File.exist?("#{root}/application.rb") ? "#{root}/application.rb" : "#{root}/environment.rb"
 
 require "rails/commands"
